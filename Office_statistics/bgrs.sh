70bc34a890f2c2c8c5a0aa14408cc96d5494c074
acedia <1694504416@qq.com>
2025-09-04 17:44:07 +0800

    无

---
+++ Office_statistics/bgrs.sh
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/acedia-x/Linux-shell/blob/main/bgrs.sh
+IDList=
+HotKey=0
+IconFile=C:\Users\z\AppData\Local\Mozilla\Firefox\Profiles\7h5cfkd6.default-release\shortcutCache\bQ99pWVw+2BuFn6AgDPsb6jEXvxxLkuLp3LcJmI_Nqc=.ico
+IconIndex=0

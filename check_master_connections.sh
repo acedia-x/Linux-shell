af26624a82fb063908c497dec4ee62c6c89e160d
acedia <1694504416@qq.com>
2025-09-08 19:59:57 +0800

    编写脚本，检测主库的连接数，连接数超时10显示警告，否则显示正常

--- check_master_connections.sh
+++ check_master_connections.sh
@@ -2,7 +2,7 @@
 
 CONNECTIONS=$(mysql -uroot -pWWW.1.com -h 2> /dev/null -e "SHOW STATUS LIKE 'Threads_connected'" | awk 'NR==2 {print $2}' )
 if [ "$CONNECTIONS" -gt 10 ]; then
-    echo "警告"
+    echo "警告 连接数超过10"
 else
     echo "正常"
 fi
